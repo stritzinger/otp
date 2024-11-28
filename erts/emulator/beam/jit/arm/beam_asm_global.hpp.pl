3e153b9d29f3260a63707a2c953ae9abff222c54
Mirjam Friesen <mirjam.friesen@stritzinger.com>
2024-11-28 15:23:40 +0100

    Adjust JIT for arm 32
    Co-authored-by: Luca Succi <luca.succi@stritzinger.com>

--- erts/emulator/beam/jit/arm/beam_asm_global.hpp.pl
+++ erts/emulator/beam/jit/arm/beam_asm_global.hpp.pl
@@ -2,7 +2,7 @@
 #
 # %CopyrightBegin%
 #
-# Copyright Ericsson AB 2022-2024. All Rights Reserved.
+# Copyright Ericsson AB 2022-2023. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -22,102 +22,13 @@ use strict;
 
 # Please keep the names in the list in alphabetical order.
 my @beam_global_funcs = qw(
-    apply_fun_shared
-    arith_compare_shared
-    bif_nif_epilogue
     bif_export_trap
-    bif_bit_size_body
-    bif_byte_size_body
-    bif_element_body_shared
-    bif_element_guard_shared
-    bif_is_eq_exact_shared
-    bif_is_ne_exact_shared
-    bif_tuple_size_body
-    bif_tuple_size_guard
-    bs_add_guard_shared
-    bs_add_body_shared
-    bs_create_bin_error_shared
-    bs_get_tail_shared
-    bs_get_utf8_shared
-    bs_get_utf8_short_shared
-    bs_init_bits_shared
-    bs_size_check_shared
-    call_bif_shared
-    call_light_bif_shared
-    call_nif_yield_helper
-    catch_end_shared
-    call_nif_early
-    call_nif_shared
-    check_float_error
-    construct_utf8_shared
-    debug_bp
-    dispatch_bif
-    dispatch_nif
-    dispatch_return
     dispatch_save_calls_export
     dispatch_save_calls_fun
     export_trampoline
-    fconv_shared
-    get_sint64_shared
-    handle_and_error
-    handle_call_fun_error
-    handle_element_error_shared
-    handle_hd_error
-    handle_map_get_badkey
-    handle_map_get_badmap
-    handle_map_size_error
-    handle_node_error
-    handle_not_error
-    handle_or_error
-    handle_tl_error
-    garbage_collect
-    generic_bp_global
-    generic_bp_local
-    i_band_body_shared
-    i_bnot_body_shared
-    i_bnot_guard_shared
-    i_bor_body_shared
-    i_bif_body_shared
-    i_bif_guard_shared
-    i_breakpoint_trampoline_shared
-    i_bsr_body_shared
-    i_bsl_body_shared
-    i_func_info_shared
-    i_get_map_element_shared
-    i_get_map_element_hash_shared
-    i_length_guard_shared
-    i_length_body_shared
-    i_loop_rec_shared
-    i_test_yield_shared
-    i_bxor_body_shared
-    int128_to_big_shared
-    int_div_rem_body_shared
-    int_div_rem_guard_shared
-    is_eq_exact_list_shared
-    is_eq_exact_shallow_boxed_shared
-    is_in_range_shared
-    is_ge_lt_shared
-    minus_body_shared
-    mul_add_body_shared
-    mul_add_guard_shared
-    mul_body_shared
-    mul_guard_shared
-    new_map_shared
-    plus_body_shared
     process_exit
     process_main
-    raise_exception
-    raise_exception_null_exp
-    raise_exception_shared
-    raise_shared
-    store_unaligned
     unloaded_fun
-    unary_minus_body_shared
-    update_map_assoc_shared
-    update_map_single_assoc_shared
-    update_map_exact_guard_shared
-    update_map_exact_body_shared
-    update_map_single_exact_body_shared
     );
 
 
@@ -196,6 +107,7 @@ $decl_emit_funcs
     void emit_raise_badarg(const ErtsCodeMFA *mfa);
 
     void emit_bif_bit_size_helper(Label fail);
+    void emit_bif_byte_size_helper(Label fail);
     void emit_bif_element_helper(Label fail);
     void emit_bif_tuple_size_helper(Label fail);
 
