2c7dd1bc1e722757989a91b11cc98ed0cc6df3fc
Luca Succi <luca.succi@stritzinger.com>
2025-01-23 13:06:00 +0100

    Fix register mapping to comply with agreed table

--- erts/emulator/beam/jit/arm/32/beam_asm.hpp
+++ erts/emulator/beam/jit/arm/32/beam_asm.hpp
@@ -77,20 +77,20 @@ protected:
 
     /* Points at x_reg_array inside an ErtsSchedulerRegisters struct, allowing
      * the aux_regs field to be addressed with an 8-bit displacement. */
-    const a32::Gp scheduler_registers = a32::r10;
+    const a32::Gp scheduler_registers = a32::r4;
 
-    const a32::Gp E = a32::fp;
+    const a32::Gp E = a32::r7;
 
-    //const a32::Gp c_p = a32::r7;
-    //const a32::Gp FCALLS = a32::r8;
-    //const a32::Gp HTOP = a32::r9;
+    const a32::Gp c_p = a32::r8;
+    const a32::Gp FCALLS = a32::r9;
+    const a32::Gp HTOP = a32::r10;
 
     /* Local copy of the active code index.
      *
      * This is set to ERTS_SAVE_CALLS_CODE_IX when save_calls is active, which
      * routes us to a common handler routine that calls save_calls before
      * jumping to the actual code. */
-    const a32::Gp active_code_ix = a32::r6;
+    const a32::Gp active_code_ix = a32::r5;
 
     /*
      * All of the following registers are caller-save.
@@ -101,6 +101,8 @@ protected:
     const a32::Gp ARG2 = a32::r1;
     const a32::Gp ARG3 = a32::r2;
     const a32::Gp ARG4 = a32::r3;
+
+    const a32::Gp VAR = a32::r6;
 
 #ifdef ERTS_MSACC_EXTENDED_STATES
     const arm::Mem erts_msacc_cache = getSchedulerRegRef(
