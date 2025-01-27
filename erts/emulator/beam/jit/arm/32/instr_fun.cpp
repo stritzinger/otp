c2f65f71f272ce57d2d37b5dc14f92f28c4db554
Luca Succi <luca.succi@stritzinger.com>
2025-01-27 15:00:03 +0100

    load 2 consecutive words from memory
    Use ldmia to Increase the pointer AFTER each read, first word goes in TMP, second in ARG1

--- erts/emulator/beam/jit/arm/32/instr_fun.cpp
+++ erts/emulator/beam/jit/arm/32/instr_fun.cpp
@@ -112,6 +112,7 @@ void BeamGlobalAssembler::emit_apply_fun_shared() {
 
             emit_ptr_val(ARG1, ARG1);
             a.sub(ARG1, ARG1, imm(TAG_PRIMARY_LIST));
+            a.ldmia(arm::Mem(ARG1), a32::GpList({TMP, ARG1}));
             //a.str(TMP3, arm::Mem(TMP2).post(sizeof(Eterm)));
 
             /* We bail at MAX_REG-1 rather than MAX_REG as the highest register
